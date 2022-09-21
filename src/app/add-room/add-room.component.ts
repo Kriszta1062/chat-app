b344ad404302f25d28904c3c6639355d165ebfe1
Kriszta1062 <kriszta1062@gmail.com>
2022-09-21 12:51:55 +0200

    Add room close button bug fixed

--- src/app/add-room/add-room.component.ts
+++ src/app/add-room/add-room.component.ts
@@ -1,20 +1,19 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RoomService } from '../services/room.service';
 import { Room } from '../models/room';
-import {faTimes} from '@fortawesome/free-solid-svg-icons';;
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-room',
   templateUrl: './add-room.component.html',
-  styleUrls: ['./add-room.component.css']
+  styleUrls: ['./add-room.component.css'],
 })
 export class AddRoomComponent implements OnInit {
-
-  faTimes=faTimes;
+  faTimes = faTimes;
 
   @Input() addButtonActive?: boolean;
-  // @Output() changedButtonActivity = new EventEmitter<boolean>();
+  @Output() changedButtonActivity = new EventEmitter<boolean>();
+  emailList!: string;
 
   room: Room = {
     name: '',
@@ -22,31 +21,31 @@ export class AddRoomComponent implements OnInit {
     // active: true,
     access: '',
     pic: '',
-  }
-
+  };
 
   constructor(private roomService: RoomService) {}
 
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  onSubmit(){
-    if(this.room.access != '' && this.room.name != '' && this.room.pic != '' && this.room.members != []){
+  onSubmit() {
+    if (
+      this.room.access != '' &&
+      this.room.name != '' &&
+      this.room.pic != '' &&
+      this.emailList != ''
+    ) {
+      this.room.members = this.emailList.split(' ');
       this.roomService.addRoom(this.room);
       this.room.name = '';
       this.room.pic = '';
-      this.room.members = [];
       this.room.access = '';
-      // this.room.active = true;
+      this.room.members = [];
 
+      // this.room.active = true;
     }
   }
 
-
-  clearAddState(){
-    this.addButtonActive = false;
-    console.log(this.addButtonActive);
+  buttonChange() {
+    this.changedButtonActivity.emit(false);
   }
-
 }
